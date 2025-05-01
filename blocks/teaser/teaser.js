5cd79ee56b87c59f5f6b52b48eead8148a0f4c62
James Zhou <james.zhou.quan@gmail.com>
2025-05-01 10:54:00 +1000

    Update header, hero, and teaser blocks with styling and functionality improvements

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -2,24 +2,28 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  // Add hero-content class to second div
-  const contentDiv = block.children[1];
-  contentDiv.className = 'hero-content';
-
-  // Add hero-image class to first div
+  // Add teaser-image class to first div and its first child div
   const imageDiv = block.firstElementChild;
-  imageDiv.className = 'hero-image';
+  imageDiv.className = 'teaser-image';
+
+  // Add teaser-content class to second div and its first child div 
+  const contentDiv = block.children[1];
+  contentDiv.className = 'teaser-content';
 
   // Add classes to content elements
   const h2 = contentDiv.querySelector('h2');
   if (h2) {
-    h2.className = 'hero-title';
+    h2.className = 'teaser-title';
   }
 
   const paragraphs = contentDiv.querySelectorAll('p');
   if (paragraphs.length > 0) {
-    paragraphs[0].className = 'hero-description';
+    paragraphs[0].className = 'teaser-description';
+    if (paragraphs[1]) {
+      paragraphs[1].className = 'teaser-link';
+    }
   }
+
 
   // Optimize images
   block.querySelectorAll('picture > img').forEach((img) => {
